a768468221ff19da4717e621644be80ddd9d870c
angkonnath1 <145487293+angkon101@users.noreply.github.com>
2024-06-06 21:26:42 +0600

    Add files via upload

---
+++ CP_CHECK_2.cpp
@@ -0,0 +1,46 @@
+/*              
+                Angkon Debnath
+                    IUBAT
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define nl endl
+
+
+
+#define N 1e6
+
+
+
+void angkon_taste_the_code()
+{
+    string s;
+    cin>>s;
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i])
+    }
+}
+
+
+
+
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--){
+        angkon_taste_the_code();
+
+   }
+
+//}
+}
+
+
+
